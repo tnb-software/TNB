2e64f799e166f25d15f6b6f3def34759796cf949
tplevko <tplevko@redhat.com>
2021-04-29 11:24:57 +0200

    [FUSEQE-12460] Add AWS Kinesis support

---
+++ system-x/services/kinesis/src/main/java/org/jboss/fuse/tnb/kinesis/service/Kinesis.java
@@ -0,0 +1,66 @@
+package org.jboss.fuse.tnb.kinesis.service;
+
+import org.jboss.fuse.tnb.aws.account.AWSAccount;
+import org.jboss.fuse.tnb.common.account.Accounts;
+import org.jboss.fuse.tnb.common.service.Service;
+import org.jboss.fuse.tnb.kinesis.validation.KinesisValidation;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.auto.service.AutoService;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.kinesis.KinesisClient;
+
+@AutoService(Kinesis.class)
+public class Kinesis implements Service {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Kinesis.class);
+
+    private AWSAccount account;
+    private KinesisClient client;
+    private KinesisValidation validation;
+
+    public AWSAccount account() {
+        if (account == null) {
+            LOG.debug("Creating new Kinesis account");
+            account = Accounts.get(AWSAccount.class);
+        }
+        return account;
+    }
+
+    public KinesisClient client() {
+        if (client == null) {
+            LOG.debug("Creating new Kinesis client");
+            client = KinesisClient.builder()
+                .region(Region.of(account().region()))
+                .credentialsProvider(() -> AwsBasicCredentials.create(account.accessKey(), account().secretKey()))
+                .build();
+        }
+        return client;
+    }
+
+    public KinesisValidation validation() {
+        if (validation == null) {
+            LOG.debug("Creating new Kinesis validation");
+            validation = new KinesisValidation(client(), account());
+        }
+        return validation;
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) throws Exception {
+        if (client != null) {
+            client.close();
+        }
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) throws Exception {
+        // no-op
+    }
+}
