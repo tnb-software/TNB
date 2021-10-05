677388a8683c44bee3ce9888b2de4b01f5346007
Andrej Vano <avano@redhat.com>
2021-10-05 09:31:05 +0200

    [Properties] Check if the test properties is set and if the file exists

--- common/src/main/java/org/jboss/fuse/tnb/common/config/provider/TestPropertiesConfigSourceProvider.java
+++ common/src/main/java/org/jboss/fuse/tnb/common/config/provider/TestPropertiesConfigSourceProvider.java
@@ -3,22 +3,30 @@ package org.jboss.fuse.tnb.common.config.provider;
 import org.apache.geronimo.config.configsource.PropertyFileConfigSource;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 import org.eclipse.microprofile.config.spi.ConfigSourceProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Paths;
 import java.util.Collections;
 
 public class TestPropertiesConfigSourceProvider implements ConfigSourceProvider {
-
+    private static final Logger LOG = LoggerFactory.getLogger(TestPropertiesConfigSourceProvider.class);
     private static final String TEST_PROPERTIES = "test.properties";
 
     @Override
     public Iterable<ConfigSource> getConfigSources(ClassLoader classLoader) {
         try {
-            URL file = Paths.get(System.getProperty(TEST_PROPERTIES, "test.properties")).toUri().toURL();
-            PropertyFileConfigSource propertyFileConfigSource = new PropertyFileConfigSource(file);
-            return Collections.singletonList(propertyFileConfigSource);
+            if (System.getProperty(TEST_PROPERTIES) == null) {
+                return Collections.emptyList();
+            }
+            File testProperties = new File(System.getProperty(TEST_PROPERTIES));
+            if (!testProperties.exists()) {
+                LOG.warn("Test properties file {} defined in {} system property does not exist!",
+                    System.getProperty(TEST_PROPERTIES), TEST_PROPERTIES);
+                return Collections.emptyList();
+            }
+            return Collections.singletonList(new PropertyFileConfigSource(testProperties.toURI().toURL()));
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
