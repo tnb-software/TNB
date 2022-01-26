0a6510c73714d3606fec9db3a5c4a92a52442409
Andrej Vano <avano@redhat.com>
2022-01-26 17:27:08 +0100

    [Integrations] Allow to create multiple integrations for all products

--- fuse-products/src/main/java/org/jboss/fuse/tnb/product/Product.java
+++ fuse-products/src/main/java/org/jboss/fuse/tnb/product/Product.java
@@ -4,17 +4,41 @@ import org.jboss.fuse.tnb.product.application.App;
 import org.jboss.fuse.tnb.product.integration.IntegrationBuilder;
 
 import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
 
-public abstract class Product implements BeforeAllCallback, AfterAllCallback, AfterEachCallback {
-    public abstract App createIntegration(IntegrationBuilder integrationBuilder);
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    public abstract void removeIntegrations();
+public abstract class Product implements BeforeAllCallback, AfterAllCallback {
+    protected Map<String, App> integrations = new ConcurrentHashMap<>();
 
-    public void afterEach(ExtensionContext extensionContext) throws Exception {
-        removeIntegrations();
+    public App createIntegration(IntegrationBuilder integrationBuilder) {
+        if (integrations.containsKey(integrationBuilder.getIntegrationName())) {
+            throw new IllegalArgumentException("Integration with name " + integrationBuilder.getIntegrationName() + " is already running!");
+        }
+        App app = createIntegrationApp(integrationBuilder);
+        integrations.put(integrationBuilder.getIntegrationName(), app);
+        return app;
+    }
+
+    public Map<String, App> createIntegration(IntegrationBuilder integrationBuilder, IntegrationBuilder... other) {
+        // Return only integrations created in this invocation, not all created integrations
+        return Stream.concat(Stream.of(integrationBuilder), Arrays.stream(other))
+            .collect(Collectors.toMap(IntegrationBuilder::getIntegrationName, this::createIntegration));
+    }
+
+    protected abstract App createIntegrationApp(IntegrationBuilder integrationBuilder);
+
+    public void removeIntegrations() {
+        integrations.values().forEach(App::stop);
+        integrations.clear();
+    }
+
+    public Map<String, App> getIntegrations() {
+        return integrations;
     }
 
     public abstract void setupProduct();
--- fuse-products/src/main/java/org/jboss/fuse/tnb/product/csb/LocalCamelSpringBoot.java
+++ fuse-products/src/main/java/org/jboss/fuse/tnb/product/csb/LocalCamelSpringBoot.java
@@ -6,27 +6,15 @@ import org.jboss.fuse.tnb.product.application.App;
 import org.jboss.fuse.tnb.product.csb.application.LocalSpringBootApp;
 import org.jboss.fuse.tnb.product.integration.IntegrationBuilder;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.auto.service.AutoService;
 
 @AutoService(Product.class)
 public class LocalCamelSpringBoot extends LocalProduct {
-    private static final Logger LOG = LoggerFactory.getLogger(LocalCamelSpringBoot.class);
-    private App app;
-
-    public App createIntegration(IntegrationBuilder integrationBuilder) {
-        app = new LocalSpringBootApp(integrationBuilder);
+    @Override
+    public App createIntegrationApp(IntegrationBuilder integrationBuilder) {
+        App app = new LocalSpringBootApp(integrationBuilder);
         app.start();
         app.waitUntilReady();
         return app;
-    }
-
-    @Override
-    public void removeIntegrations() {
-        if (app != null) {
-            app.stop();
-        }
     }
 }
