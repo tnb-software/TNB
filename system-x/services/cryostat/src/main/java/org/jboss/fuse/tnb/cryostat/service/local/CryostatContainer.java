8fc4726abb69b2a034d19e6dfaf8370e7a8da45c
Marco Carletti <mcarlett@redhat.com>
2022-05-11 16:44:12 +0200

    👽️ Manage new Cryostat image

--- system-x/services/cryostat/src/main/java/org/jboss/fuse/tnb/cryostat/service/local/CryostatContainer.java
+++ system-x/services/cryostat/src/main/java/org/jboss/fuse/tnb/cryostat/service/local/CryostatContainer.java
@@ -8,9 +8,13 @@ public class CryostatContainer extends GenericContainer<CryostatContainer> {
 
     public static final String IMAGE = System.getProperty("cryostat.image", "registry.redhat.io/cryostat-20-tech-preview/cryostat-rhel8:latest");
 
+    private static final String TMP_FOLDER = System.getProperty("java.io.tmpdir");
+
     public CryostatContainer(Map<String, String> env, int... exposedPorts) {
         super(IMAGE);
-        env.put("CRYOSTAT_CONFIG_PATH", "/tmp");
+        env.put("CRYOSTAT_CONFIG_PATH", TMP_FOLDER);
+        env.put("CRYOSTAT_PROBE_TEMPLATE_PATH", TMP_FOLDER);
+        env.put("CRYOSTAT_ARCHIVE_PATH", TMP_FOLDER);
         this.withEnv(env);
         this.withNetworkMode("host"); // using host network
     }
