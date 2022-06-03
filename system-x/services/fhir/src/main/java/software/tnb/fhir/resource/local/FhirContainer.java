3c7c94fd2e6f2fe5e09958ad7e4c4df0dd1aad46
tveskrna <tveskrna@redhat.com>
2022-06-03 14:16:51 +0200

    [CSB-445] Implement fhir container

---
+++ system-x/services/fhir/src/main/java/software/tnb/fhir/resource/local/FhirContainer.java
@@ -0,0 +1,16 @@
+package software.tnb.fhir.resource.local;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.util.Map;
+
+public class FhirContainer extends GenericContainer<FhirContainer> {
+
+    public FhirContainer(String image, int port, Map<String, String> env) {
+        super(image);
+        withExposedPorts(port);
+        withEnv(env);
+        waitingFor(Wait.forHttp("/hapi-fhir-jpaserver/fhir/metadata"));
+    }
+}
---
+++ system-x/services/fhir/src/main/java/software/tnb/fhir/resource/local/LocalFhir.java
@@ -0,0 +1,54 @@
+package software.tnb.fhir.resource.local;
+
+import software.tnb.common.deployment.Deployable;
+import software.tnb.fhir.service.Fhir;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.auto.service.AutoService;
+
+@AutoService(Fhir.class)
+public class LocalFhir extends Fhir implements Deployable {
+    private static final Logger LOG = LoggerFactory.getLogger(LocalFhir.class);
+    private FhirContainer container;
+
+    @Override
+    public String defaultImage() {
+        return FHIR_IMAGE;
+    }
+
+    @Override
+    public void deploy() {
+        LOG.info("Starting FHIR container");
+        container = new FhirContainer(defaultImage(), PORT, containerEnvironment());
+        container.start();
+        LOG.info("FHIR container started");
+    }
+
+    @Override
+    public void undeploy() {
+        if (container != null) {
+            LOG.info("Stopping FHIR container");
+            container.stop();
+        }
+    }
+
+    @Override
+    public int getPortMapping() {
+        return container.getMappedPort(PORT);
+    }
+
+    @Override
+    public String getServerUrl() {
+        return "http://localhost:" + getPortMapping() + "/";
+    }
+
+    @Override
+    public void openResources() {
+    }
+
+    @Override
+    public void closeResources() {
+    }
+}
