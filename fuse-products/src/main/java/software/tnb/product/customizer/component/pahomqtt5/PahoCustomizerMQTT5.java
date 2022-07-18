5bb24c71e04a8bbd9079fe93af5128543ca42c96
Salvatore Mongiardo <smongiar@redhat.com>
2022-07-18 14:23:07 +0200

    ✨ Add camel-paho-mqtt5 system-x services

---
+++ fuse-products/src/main/java/software/tnb/product/customizer/component/pahomqtt5/PahoCustomizerMQTT5.java
@@ -0,0 +1,45 @@
+package software.tnb.product.customizer.component.pahomqtt5;
+
+import software.tnb.product.customizer.ProductsCustomizer;
+
+import java.util.Map;
+
+public class PahoCustomizerMQTT5 extends ProductsCustomizer {
+
+    private final String username;
+    private final String password;
+    private final String brokerUrl;
+
+    public PahoCustomizerMQTT5(String username, String password, String brokerUrl) {
+        this.brokerUrl = brokerUrl;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public void customizeCamelK() {
+        customizeCEQAndCK();
+    }
+
+    @Override
+    public void customizeQuarkus() {
+        customizeCEQAndCK();
+    }
+
+    private void customizeCEQAndCK() {
+        getIntegrationBuilder().addToProperties(Map.of(
+            "camel.component.paho-mqtt5.username", username,
+            "camel.component.paho-mqtt5.password", password,
+            "paho-mqtt5.broker.tcp.url", brokerUrl
+        ));
+    }
+
+    @Override
+    public void customizeSpringboot() {
+        getIntegrationBuilder().addToProperties(Map.of(
+            "camel.component.paho-mqtt5.user-name", username,
+            "camel.component.paho-mqtt5.password", password,
+            "camel.component.paho-mqtt5.broker-url", brokerUrl
+        ));
+    }
+}
