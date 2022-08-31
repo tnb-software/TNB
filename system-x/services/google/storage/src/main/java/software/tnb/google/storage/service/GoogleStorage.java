b33c2378d38360296449eaa362049331d53d45f6
Andrej Vano <avano@redhat.com>
2022-08-31 15:39:38 +0200

    [GoogleStorage] Add more logging

--- system-x/services/google/storage/src/main/java/software/tnb/google/storage/service/GoogleStorage.java
+++ system-x/services/google/storage/src/main/java/software/tnb/google/storage/service/GoogleStorage.java
@@ -8,6 +8,8 @@ import software.tnb.google.storage.validation.GoogleStorageValidation;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.auto.service.AutoService;
@@ -18,6 +20,8 @@ import io.fabric8.kubernetes.client.utils.Base64;
 
 @AutoService(GoogleStorage.class)
 public class GoogleStorage implements Service {
+    private static final Logger LOG = LoggerFactory.getLogger(GoogleStorage.class);
+
     private GoogleCloudAccount account;
     private Storage client;
     private GoogleStorageValidation validation;
@@ -31,6 +35,7 @@ public class GoogleStorage implements Service {
 
     protected Storage client() {
         if (client == null) {
+            LOG.debug("Creating new Google Storage client");
             try {
                 String decodedJson = new String(Base64.decode(account().serviceAccountKey()));
                 client = StorageOptions.newBuilder().setCredentials(
@@ -53,6 +58,7 @@ public class GoogleStorage implements Service {
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
+        LOG.debug("Creating new Google Storage validation");
         validation = new GoogleStorageValidation(client());
     }
 }
