d93675554136d962a0ef2a863d3db47581893b39
Andrej Vano <avano@redhat.com>
2022-09-07 13:23:46 +0200

    [MGDCTRS-1271] Add webhook system-x

---
+++ system-x/services/webhook/src/main/java/software/tnb/webhook/validation/WebhookValidation.java
@@ -0,0 +1,53 @@
+package software.tnb.webhook.validation;
+
+import software.tnb.common.utils.HTTPUtils;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+
+public class WebhookValidation {
+    private static final Logger LOG = LoggerFactory.getLogger(WebhookValidation.class);
+    private static final String WEBHOOK_ENDPOINT = "https://webhook.site";
+
+    private final HTTPUtils client;
+    private String token;
+
+    public WebhookValidation() {
+        client = HTTPUtils.getInstance();
+    }
+
+    public String createEndpoint() {
+        LOG.debug("Creating new webhook endpoint");
+        token = new JSONObject(client.post(WEBHOOK_ENDPOINT + "/token", RequestBody.create(MediaType.parse("application/json"), "{}")).getBody())
+            .getString("uuid");
+        final String endpoint = String.format("%s/%s", WEBHOOK_ENDPOINT, token);
+        LOG.debug("Webhook endpoint: {}", endpoint);
+        return endpoint;
+    }
+
+    public void clearEndpoint() {
+        final String url = String.format("%s/token/%s/request", WEBHOOK_ENDPOINT, token);
+        LOG.debug("Cleaning endpoint {}", url);
+        client.delete(url);
+    }
+
+    public void deleteEndpoint() {
+        final String url = String.format("%s/token/%s", WEBHOOK_ENDPOINT, token);
+        LOG.debug("Deleting webhook endpoint {}", url);
+        client.delete(url);
+    }
+
+    public List<JSONObject> getRequests() {
+        final String url = String.format("%s/token/%s/requests", WEBHOOK_ENDPOINT, token);
+        return new JSONObject(client.get(url).getBody()).getJSONArray("data")
+            .toList().stream().map(o -> new JSONObject((Map) o)).collect(Collectors.toList());
+    }
+}
