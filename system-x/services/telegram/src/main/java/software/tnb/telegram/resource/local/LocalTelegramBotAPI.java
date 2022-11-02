13116d6c14bf525ffde6b28d2ba94516318577b5
Marco Carletti <mcarlett@redhat.com>
2022-11-02 16:03:42 +0100

    ✨ Make Telegram Bot API deployable on OCP

---
+++ system-x/services/telegram/src/main/java/software/tnb/telegram/resource/local/LocalTelegramBotAPI.java
@@ -0,0 +1,59 @@
+package software.tnb.telegram.resource.local;
+
+import software.tnb.common.deployment.Deployable;
+import software.tnb.telegram.service.TelegramBotApi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.auto.service.AutoService;
+
+@AutoService(TelegramBotApi.class)
+public class LocalTelegramBotAPI extends TelegramBotApi implements Deployable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LocalTelegramBotAPI.class);
+
+    private TelegramBotAPIContainer container;
+
+    @Override
+    public String externalHostname() {
+        return "localhost";
+    }
+
+    @Override
+    protected String getWorkingDir() {
+        return "/home/telegram-bot-api/telegram/working";
+    }
+
+    @Override
+    protected String getUploadDir() {
+        return "/home/telegram-bot-api/telegram/temp-files";
+    }
+
+    @Override
+    public String getLogs() {
+        return container.getLogs();
+    }
+
+    @Override
+    public void deploy() {
+        LOG.debug("Starting Telegram Bot API container");
+        container = new TelegramBotAPIContainer(image(), getEnv(), startupParams(), getPort());
+        container.start();
+    }
+
+    @Override
+    public void undeploy() {
+        container.stop();
+    }
+
+    @Override
+    public void openResources() {
+
+    }
+
+    @Override
+    public void closeResources() {
+
+    }
+}
--- system-x/services/telegram/src/main/java/software/tnb/telegram/service/TelegramBotApi.java
+++ system-x/services/telegram/src/main/java/software/tnb/telegram/service/TelegramBotApi.java
@@ -1,62 +1,52 @@
 package software.tnb.telegram.service;
 
 import software.tnb.common.account.AccountFactory;
+import software.tnb.common.deployment.WithDockerImage;
 import software.tnb.common.deployment.WithExternalHostname;
 import software.tnb.common.service.Service;
 import software.tnb.telegram.account.TelegramAccount;
-import software.tnb.telegram.resource.TelegramBotAPIContainer;
-
-import org.junit.jupiter.api.extension.ExtensionContext;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.auto.service.AutoService;
 
 import java.util.Map;
 
-@AutoService(TelegramBotApi.class)
-public class TelegramBotApi implements Service, WithExternalHostname {
+public abstract class TelegramBotApi implements Service, WithExternalHostname, WithDockerImage {
+
+    private static final String PORT = System.getProperty("telegram-bot-api.port", "8081");
+    private static final String VERBOSITY = System.getProperty("telegram-bot-api.verbosity", "9");
     private TelegramAccount account;
-    private TelegramBotAPIContainer container;
-    private static final Logger LOG = LoggerFactory.getLogger(TelegramBotApi.class);
 
-    @Override
-    public void beforeAll(ExtensionContext context) {
-        LOG.debug("Creating new Telegram Bot API validation");
-        account(); //initialize account
-        container = new TelegramBotAPIContainer(getEnv()); //init container
-        container.start();
-    }
-
-    public TelegramAccount account() {
+    protected TelegramAccount account() {
         if (account == null) {
-            LOG.debug("Creating new Telegram Bot API account");
             account = AccountFactory.create(TelegramAccount.class);
         }
         return account;
     }
 
-    @Override
-    public void afterAll(ExtensionContext context) {
-        container.stop();
+    protected Map<String, String> getEnv() {
+        return Map.of("TELEGRAM_API_ID", account().getAppId(),
+            "TELEGRAM_API_HASH", account().getAppHash());
     }
 
-    public Map<String, String> getEnv() {
-        return Map.of("TELEGRAM_API_ID", account.getAppId(),
-            "TELEGRAM_API_HASH", account.getAppHash());
-    }
-
-    @Override
-    public String externalHostname() {
-        return "localhost";
+    protected String[] startupParams() {
+        return new String[] {"--local", "--http-port", PORT
+            , "--dir", getWorkingDir()
+            , "--temp-dir", getUploadDir()
+            , "--http-ip-address", "0.0.0.0"
+            , "--verbosity", VERBOSITY};
     }
 
     public int getPort() {
-        return container.getPort();
+        return Integer.parseInt(PORT);
     }
 
-    public String getLogs() {
-        return container.getLogs();
+    @Override
+    public String defaultImage() {
+        return "quay.io/fuse_qe/telegram-bot-api:latest";
     }
+
+    public abstract String getLogs();
+
+    protected abstract String getWorkingDir();
+
+    protected abstract String getUploadDir();
+
 }
