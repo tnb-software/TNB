cfce3c1133626fc9132a13d4d3c58815a480f7aa
Andrej Vano <avano@redhat.com>
2023-09-14 10:34:20 +0200

    [Camel] Remove unused property

--- fuse-products/src/main/java/software/tnb/product/camel/CamelConfiguration.java
+++ fuse-products/src/main/java/software/tnb/product/camel/CamelConfiguration.java
@@ -4,13 +4,8 @@ import software.tnb.common.config.Configuration;
 
 public class CamelConfiguration extends Configuration {
     public static final String CAMEL_VERSION = "camel.version";
-    public static final String CAMEL_ARCHETYPE_VERSION = "camel.archetype.version";
 
     public static String camelVersion() {
         return getProperty(CAMEL_VERSION);
-    }
-
-    public static String camelArchetypeVersion() {
-        return getProperty(CAMEL_ARCHETYPE_VERSION);
     }
 }
