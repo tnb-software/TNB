9a5f20711a6a2b0b2c371dd59f9ce98e808ac9a6
Andrej Vano <avano@redhat.com>
2023-09-15 09:45:02 +0200

    [Jira] Add product config enum for whole quarkus / sb

--- fuse-products/src/main/java/software/tnb/product/configuration/ProductConfiguration.java
+++ fuse-products/src/main/java/software/tnb/product/configuration/ProductConfiguration.java
@@ -6,35 +6,34 @@ import software.tnb.common.product.ProductType;
 import software.tnb.product.cq.configuration.QuarkusConfiguration;
 
 public enum ProductConfiguration {
-    ALL, QUARKUS_JVM, QUARKUS_JVM_LOCAL, QUARKUS_JVM_OPENSHIFT, QUARKUS_NATIVE, QUARKUS_NATIVE_LOCAL, QUARKUS_NATIVE_OPENSHIFT, CAMEL_K,
-        SPRINGBOOT_JVM_LOCAL, SPRINGBOOT_JVM_OPENSHIFT;
+    ALL,
+    QUARKUS, QUARKUS_JVM, QUARKUS_JVM_LOCAL, QUARKUS_JVM_OPENSHIFT, QUARKUS_NATIVE, QUARKUS_NATIVE_LOCAL, QUARKUS_NATIVE_OPENSHIFT,
+    CAMEL_K,
+    SPRINGBOOT, SPRINGBOOT_JVM_LOCAL, SPRINGBOOT_JVM_OPENSHIFT;
 
     public boolean isCurrentEnv() {
-        switch (this) {
-            case QUARKUS_JVM:
-                return TestConfiguration.product() == ProductType.CAMEL_QUARKUS && !QuarkusConfiguration.isQuarkusNative();
-            case QUARKUS_JVM_LOCAL:
-                return TestConfiguration.product() == ProductType.CAMEL_QUARKUS && !QuarkusConfiguration.isQuarkusNative() && !OpenshiftConfiguration
-                    .isOpenshift();
-            case QUARKUS_JVM_OPENSHIFT:
-                return TestConfiguration.product() == ProductType.CAMEL_QUARKUS && !QuarkusConfiguration.isQuarkusNative() && OpenshiftConfiguration
-                    .isOpenshift();
-            case QUARKUS_NATIVE:
-                return TestConfiguration.product() == ProductType.CAMEL_QUARKUS && QuarkusConfiguration.isQuarkusNative();
-            case QUARKUS_NATIVE_LOCAL:
-                return TestConfiguration.product() == ProductType.CAMEL_QUARKUS && QuarkusConfiguration.isQuarkusNative() && !OpenshiftConfiguration
-                    .isOpenshift();
-            case QUARKUS_NATIVE_OPENSHIFT:
-                return TestConfiguration.product() == ProductType.CAMEL_QUARKUS && QuarkusConfiguration.isQuarkusNative() && OpenshiftConfiguration
-                    .isOpenshift();
-            case CAMEL_K:
-                return TestConfiguration.product() == ProductType.CAMEL_K;
-            case SPRINGBOOT_JVM_LOCAL:
-                return TestConfiguration.product() == ProductType.CAMEL_SPRINGBOOT && !OpenshiftConfiguration.isOpenshift();
-            case SPRINGBOOT_JVM_OPENSHIFT:
-                return TestConfiguration.product() == ProductType.CAMEL_SPRINGBOOT && OpenshiftConfiguration.isOpenshift();
-            default:
-                return true;
-        }
+        return switch (this) {
+            case QUARKUS -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS;
+            case QUARKUS_JVM -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS
+                && !QuarkusConfiguration.isQuarkusNative();
+            case QUARKUS_JVM_LOCAL -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS
+                && !QuarkusConfiguration.isQuarkusNative() && !OpenshiftConfiguration.isOpenshift();
+            case QUARKUS_JVM_OPENSHIFT -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS
+                && !QuarkusConfiguration.isQuarkusNative() && OpenshiftConfiguration.isOpenshift();
+            case QUARKUS_NATIVE -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS
+                && QuarkusConfiguration.isQuarkusNative();
+            case QUARKUS_NATIVE_LOCAL -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS
+                && QuarkusConfiguration.isQuarkusNative() && !OpenshiftConfiguration.isOpenshift();
+            case QUARKUS_NATIVE_OPENSHIFT -> TestConfiguration.product() == ProductType.CAMEL_QUARKUS
+                && QuarkusConfiguration.isQuarkusNative()
+                && OpenshiftConfiguration.isOpenshift();
+            case CAMEL_K -> TestConfiguration.product() == ProductType.CAMEL_K;
+            case SPRINGBOOT -> TestConfiguration.product() == ProductType.CAMEL_SPRINGBOOT;
+            case SPRINGBOOT_JVM_LOCAL -> TestConfiguration.product() == ProductType.CAMEL_SPRINGBOOT
+                && !OpenshiftConfiguration.isOpenshift();
+            case SPRINGBOOT_JVM_OPENSHIFT -> TestConfiguration.product() == ProductType.CAMEL_SPRINGBOOT
+                && OpenshiftConfiguration.isOpenshift();
+            default -> true;
+        };
     }
 }
