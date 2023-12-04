d21e9b3f7da5bbd3a94501d44e90f5ff4ae9d8ea
Andrej Vano <avano@redhat.com>
2023-12-04 16:47:40 +0100

    [Filesystem] Add interfaces to resources

--- system-x/services/filesystem/src/main/java/software/tnb/filesystem/resource/local/LocalFileSystem.java
+++ system-x/services/filesystem/src/main/java/software/tnb/filesystem/resource/local/LocalFileSystem.java
@@ -1,16 +1,15 @@
 package software.tnb.filesystem.resource.local;
 
+import software.tnb.common.deployment.Deployable;
 import software.tnb.common.utils.IOUtils;
 import software.tnb.filesystem.service.FileSystem;
-
-import org.junit.jupiter.api.extension.ExtensionContext;
 
 import com.google.auto.service.AutoService;
 
 import java.nio.file.Path;
 
 @AutoService(FileSystem.class)
-public class LocalFileSystem extends FileSystem {
+public class LocalFileSystem extends FileSystem implements Deployable {
     @Override
     public void setAppName(String app) {
     }
@@ -18,13 +17,5 @@ public class LocalFileSystem extends FileSystem {
     @Override
     public String getFileContent(Path path) {
         return IOUtils.readFile(path);
-    }
-
-    @Override
-    public void afterAll(ExtensionContext extensionContext) throws Exception {
-    }
-
-    @Override
-    public void beforeAll(ExtensionContext extensionContext) throws Exception {
     }
 }
