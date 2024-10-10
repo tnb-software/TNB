57b53338734a5d2ba93896555a33d0b8da1d854f
Andrej Vano <avano@redhat.com>
2024-10-10 10:48:56 +0200

    [Quarkus] Fix rest dependency name for 3.15

--- fuse-products/src/main/java/software/tnb/product/cq/application/QuarkusApp.java
+++ fuse-products/src/main/java/software/tnb/product/cq/application/QuarkusApp.java
@@ -161,7 +161,7 @@ public abstract class QuarkusApp extends App {
             // quarkus-resteasy is needed for the openshift.yml to be generated, but the resteasy itself is not used anywhere
             // remove quarkus-resteasy-reactive in local deployments as it can throw exceptions for occupied 8080 port
             model.setDependencies(
-                model.getDependencies().stream().filter(d -> !"quarkus-resteasy-reactive".equals(d.getArtifactId())).collect(Collectors.toList())
+                model.getDependencies().stream().filter(d -> !"quarkus-rest".equals(d.getArtifactId())).collect(Collectors.toList())
             );
         }
 
