058c4391362430137bcf3ed77755a6077ab77bf2
Jakub Vrubel <jvrubel@jvrubel-thinkpadt14gen4.brq.csb>
2024-10-31 12:56:25 +0100

    ADD system-x azure-key-vault service

---
+++ system-x/services/azure/key-vault/src/main/java/software/tnb/azure/keyvault/account/KeyVaultAccount.java
@@ -0,0 +1,48 @@
+package software.tnb.azure.keyvault.account;
+
+import software.tnb.common.account.Account;
+import software.tnb.common.account.WithId;
+
+public class KeyVaultAccount implements Account, WithId {
+    private String vaultName;
+    private String tenantId;
+    private String clientId;
+    private String clientSecret;
+
+    public void setVault_name(String vault_name) {
+        this.vaultName = vault_name;
+    }
+
+    public void setTenant_Id(String tenant_Id) {
+        this.tenantId = tenant_Id;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public void setClient_secret(String client_secret) {
+        this.clientSecret = client_secret;
+    }
+
+    public String tenantId() {
+        return tenantId;
+    }
+
+    public String clientId() {
+        return clientId;
+    }
+
+    public String clientSecret() {
+        return clientSecret;
+    }
+
+    public String vaultName() {
+        return vaultName;
+    }
+
+    @Override
+    public String credentialsId() {
+        return "tnb-azure-key-vault";
+    }
+}
---
+++ system-x/services/azure/key-vault/src/main/java/software/tnb/azure/keyvault/validation/KeyVaultValidation.java
@@ -0,0 +1,49 @@
+package software.tnb.azure.keyvault.validation;
+
+import software.tnb.common.validation.Validation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.azure.core.util.polling.SyncPoller;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.models.DeletedSecret;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+
+public class KeyVaultValidation implements Validation {
+    private static final Logger LOG = LoggerFactory.getLogger(KeyVaultValidation.class);
+
+    private final SecretClient client;
+
+    public KeyVaultValidation(SecretClient client) {
+        this.client = client;
+    }
+
+    public KeyVaultSecret getSecret(String secret) {
+        LOG.debug("Getting secret " + secret);
+        KeyVaultSecret value = client.getSecret(secret);
+        LOG.debug("Value read from secret {}: {}", secret, value.getValue());
+        return value;
+    }
+
+    public void setSecret(String name, String value) {
+        LOG.debug("Setting secret: {} to value: {}", name, value);
+        client.setSecret(name, value);
+    }
+
+    public void createSecret(String name, String value) {
+        LOG.debug(String.format("Creating secret: %s with value: %s", name, value));
+        KeyVaultSecret newSecret = new KeyVaultSecret(name, value);
+        client.setSecret(newSecret);
+    }
+
+    public void deleteSecret(String name) {
+        LOG.debug("Deleting secret " + name);
+        SyncPoller<DeletedSecret, Void> deletionPoller = client.beginDeleteSecret(name);
+        deletionPoller.waitForCompletion();
+    }
+
+    public void purgeDeletedSecret(String name) {
+        client.purgeDeletedSecret(name);
+    }
+}
