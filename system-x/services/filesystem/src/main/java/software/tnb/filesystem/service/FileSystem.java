43fa4183c82c36948956c166b44454a7505e5045
Croway <federico.mariani.1990@gmail.com>
2024-11-28 17:43:10 +0100

    Upload tmp file to OCP

--- system-x/services/filesystem/src/main/java/software/tnb/filesystem/service/FileSystem.java
+++ system-x/services/filesystem/src/main/java/software/tnb/filesystem/service/FileSystem.java
@@ -5,12 +5,17 @@ import software.tnb.common.client.NoClient;
 import software.tnb.common.service.Service;
 import software.tnb.common.validation.NoValidation;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public abstract class FileSystem extends Service<NoAccount, NoClient, NoValidation> {
     public abstract void setAppName(String app);
 
     public abstract String getFileContent(Path path);
+
+    public abstract boolean createFile(Path directory, String filename, String content) throws IOException;
+
+    public abstract Path createTempDirectory() throws IOException;
 
     public void deploy() {
     }
