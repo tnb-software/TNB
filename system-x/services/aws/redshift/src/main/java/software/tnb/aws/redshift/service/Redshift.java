b12d3e2128abd1786bbd1c4d4c4a51303e2008eb
Andrej Vano <avano@redhat.com>
2024-12-03 13:11:00 +0100

    [Redshift] Remove creating snapshots

--- system-x/services/aws/redshift/src/main/java/software/tnb/aws/redshift/service/Redshift.java
+++ system-x/services/aws/redshift/src/main/java/software/tnb/aws/redshift/service/Redshift.java
@@ -10,13 +10,8 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 import com.google.auto.service.AutoService;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-
 import software.amazon.awssdk.services.redshift.RedshiftClient;
 import software.amazon.awssdk.services.redshift.model.Cluster;
-import software.amazon.awssdk.services.redshift.model.DescribeClusterSnapshotsResponse;
 import software.amazon.awssdk.services.redshift.model.RedshiftException;
 import software.amazon.awssdk.services.redshiftdata.RedshiftDataClient;
 
@@ -61,7 +56,6 @@ public class Redshift extends AWSService<RedshiftAccount, RedshiftDataClient, Re
 
     private void pauseCluster() {
         if (!cluster().clusterAvailabilityStatus().equalsIgnoreCase("paused")) {
-            checkSnapshots();
             try {
                 redshiftClient.pauseCluster(builder -> builder.clusterIdentifier(account.clusterIdentifier()).build());
                 //wait for paused
@@ -71,32 +65,6 @@ public class Redshift extends AWSService<RedshiftAccount, RedshiftDataClient, Re
             } catch (RedshiftException e) {
                 throw new RuntimeException("Failed to stop redshift cluster, needs to be stopped manually");
             }
-        }
-    }
-
-    private void checkSnapshots() {
-        DescribeClusterSnapshotsResponse respRecent = redshiftClient.describeClusterSnapshots(builder -> builder
-            .clusterIdentifier(account.clusterIdentifier())
-            .startTime(Instant.now().minus(5, ChronoUnit.HOURS)) //any - manual/automated
-        );
-
-        if (respRecent.snapshots().isEmpty()) {
-            LOG.debug("Create a snapshot");
-            //needs to be created new snapshot
-            String id = "snapshot-tnb-" + new Date().getTime();
-            redshiftClient.createClusterSnapshot(builder -> builder
-                .clusterIdentifier(account.clusterIdentifier())
-                .snapshotIdentifier(id)
-                .manualSnapshotRetentionPeriod(1)); //1 day
-            //wait for snapshot to be available
-            WaitUtils.waitFor(() -> redshiftClient.describeClusterSnapshots(builder -> builder
-                    .snapshotIdentifier(id)).snapshots().get(0).status().equals("available"), 30,
-                10000, "Waiting for snapshot " + id + " to be available");
-
-            //wait till cluster leaves "Modifying" state
-            WaitUtils
-                .waitFor(() -> !cluster().clusterAvailabilityStatus().equalsIgnoreCase("modifying"), 60, 30000,
-                    "Waiting for Cluster " + cluster().clusterIdentifier() + " to process the snapshot");
         }
     }
 }
