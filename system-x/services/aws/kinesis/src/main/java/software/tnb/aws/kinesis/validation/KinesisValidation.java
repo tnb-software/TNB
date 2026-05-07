563309d2436daf7ab99e036667e55079eaef43b7
Andrej Vano <avano@redhat.com>
2026-05-07 09:45:13 +0200

    [Kinesis] Wait until there is space for stream manipulations

--- system-x/services/aws/kinesis/src/main/java/software/tnb/aws/kinesis/validation/KinesisValidation.java
+++ system-x/services/aws/kinesis/src/main/java/software/tnb/aws/kinesis/validation/KinesisValidation.java
@@ -1,5 +1,6 @@
 package software.tnb.aws.kinesis.validation;
 
+import software.tnb.common.utils.WaitUtils;
 import software.tnb.common.validation.Validation;
 
 import org.slf4j.Logger;
@@ -12,6 +13,8 @@ import software.amazon.awssdk.services.kinesis.KinesisClient;
 import software.amazon.awssdk.services.kinesis.model.GetRecordsResponse;
 import software.amazon.awssdk.services.kinesis.model.Shard;
 import software.amazon.awssdk.services.kinesis.model.ShardIteratorType;
+import software.amazon.awssdk.services.kinesis.model.StreamStatus;
+import software.amazon.awssdk.services.kinesis.model.StreamSummary;
 
 public class KinesisValidation implements Validation {
     private static final Logger LOG = LoggerFactory.getLogger(KinesisValidation.class);
@@ -24,8 +27,10 @@ public class KinesisValidation implements Validation {
 
     public void createDataStream(String name) {
         LOG.info("Creating Kinesis data stream {}", name);
-        client.createStream(b -> b.streamName(name).shardCount(1));
-        waitForDataStream(name);
+        withWaitForStreams(() -> {
+            client.createStream(b -> b.streamName(name).shardCount(1));
+            waitForDataStream(name);
+        });
     }
 
     private void waitForDataStream(String name) {
@@ -36,7 +41,10 @@ public class KinesisValidation implements Validation {
 
     public void deleteDataStream(String name) {
         LOG.info("Deleting Kinesis data stream {}", name);
-        client.deleteStream(b -> b.streamName(name));
+        withWaitForStreams(() -> {
+            client.deleteStream(b -> b.streamName(name));
+            client.waiter().waitUntilStreamNotExists(b -> b.streamName(name));
+        });
     }
 
     public void sendMessage(String stream, String message, String partitionKey) {
@@ -62,5 +70,22 @@ public class KinesisValidation implements Validation {
 
     public GetRecordsResponse getRecords(int maxNumberOfRecords, String shardIterator) {
         return client.getRecords(b -> b.shardIterator(shardIterator).limit(maxNumberOfRecords));
+    }
+
+    /**
+     * If multiple tests are running at once, we can hit an exception from kinesis stating that:
+     * This request would have exceed the limit on the number of streams that can simultaneously be in CREATING or DELETING. Limit: 5
+     * @param r runnable to run
+     */
+    private void withWaitForStreams(Runnable r) {
+        while (client.listStreams().streamSummaries()
+            .stream()
+            .map(StreamSummary::streamStatus)
+            .filter(s -> s == StreamStatus.CREATING || s == StreamStatus.DELETING)
+            .count() > 3) {
+            LOG.debug("Waiting until there is less than 3 streams in creating or deleting state");
+            WaitUtils.sleep(30000L);
+        }
+        r.run();
     }
 }
